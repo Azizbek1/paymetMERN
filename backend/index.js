305e1b5bded6fca3b69192a527f682783ed6b260
Azizbek <sdkfjdskf@asdas.com>
2022-02-01 12:22:15 +0500

    asd

--- backend/index.js
+++ backend/index.js
@@ -22,9 +22,9 @@ app.use("/api/auth", authRoutes);
 
 __dirname = path.resolve()
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '/client/build')))
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", 'public', 'index.html'))
+        res.sendFile(path.resolve(__dirname, "frontend", 'public', 'index.html'))
     })
 }else{
     app.get("/", (req,res) => {
